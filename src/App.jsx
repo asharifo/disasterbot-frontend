b02747c2d6d5bcfb0347f90dfdc7878e3b3fc522
asharifo <asharifo@students.cs.ubc.ca>
2025-12-31 09:51:50 -0800

    Structured project and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import DisasterDimensions from "./pages/DisasterDimensions";
+import Chatbot from "./pages/Chatbot";
+import { Routes, Route } from "react-router-dom";
+import MoreInfo from "./pages/MoreInfo";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<DisasterDimensions />} />
+
+          <Route path="/chatbot">
+            {/* public login route */}
+            <Route path="login" element={<Login />} />
+            {/* protected area */}
+            <Route element={<ChatbotLayout />}>
+              <Route index element={<Chatbot />} />
+            </Route>
+          </Route>
+
+          <Route path="/more-info" element={<MoreInfo />} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
